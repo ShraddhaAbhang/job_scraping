a3c72c67cc83664f70b700caf37a147b9f778f03
ShraddhaAbhang <shraddhasampatabhang@gmail.com>
2024-09-09 00:02:34 +0530

    Added notes & basic requirement

---
+++ notes.py
@@ -0,0 +1,19 @@
+'''
+# Notes:
+
+- Libraries Required:
+
+        Install requests and BeautifulSoup via pip if you haven’t already:
+                ~ pip install requests beautifulsoup4
+
+- Respect Robots.txt and Terms of Service:
+
+        Always check the robots.txt file of the website and adhere to their scraping policies.
+
+- Handle Pagination:
+        If the website has multiple pages of job listings, you may need to handle pagination by iterating through URLs or by interacting with the site’s navigation.
+
+- Dynamic Content:
+        For websites with dynamic content loaded via JavaScript, you might need a more advanced tool like Selenium or Playwright to interact with the page.
+
+'''
